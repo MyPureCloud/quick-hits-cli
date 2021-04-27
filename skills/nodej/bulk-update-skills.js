effdb26ab1b807191e4caa0ea4d9247da4f8b9d4
AgnesCorpuz <38449514+AgnesCorpuz@users.noreply.github.com>
2021-04-27 18:09:41 +0800

    Refined codes for Windows

--- skills/nodej/bulk-update-skills.js
+++ skills/nodej/bulk-update-skills.js
@@ -1,3 +1,13 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+//  DESCRIPTION
+//      Filters all users with skills in the removeSkills array and removes those skills to that user.
+//      After removing the skills, use the blukremove to add skills to the same user in bulk.
+//  PRE-REQUISITE
+//      If you are using Windows, rename your jq-win64.exe or jq-win32.exe to jq.exe
+//  NODE VERSION
+//      Node v14.16.1
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+
 const execSync = require('child_process').execSync;
 
 let removeSkills = ['Juggling','Fire Breathing','Origami'];
@@ -6,15 +16,12 @@ console.log('Working on it...');
 
 // Loop through each skill to be deleted
 removeSkills.forEach(skill => {
-    let gcBuilder = 'gc';
     let jqBuilder = 'jq -c -r \'[.[] | select(.skills[].name=="'+ skill +'")]\''
 
-    if(process.platform === 'win32') {
-        gcBuilder = 'gc.exe';
-        jqBuilder = 'jq -c -r "[.[] | select(.skills[].name==""'+ skill +'""")]"';
-    }
+    // Rename your jq-win64.exe or jq-win32.exe to jq.exe
+    if(process.platform === 'win32') jqBuilder = 'jq -c -r "[.[] | select(.skills[].name==""'+ skill +'""")]"';
 
-    userData = execSync(''+ gcBuilder +' users list -a --pageSize=100 --expand="skills" | '+ jqBuilder +'');
+    userData = execSync('gc users list -a --pageSize=100 --expand="skills" | '+ jqBuilder +'');
 
     var obj = JSON.parse(userData);
 
@@ -24,13 +31,13 @@ removeSkills.forEach(skill => {
             skillArr = user.skills.find(x => x.name == skill);
 
             // Remove skill for each user
-            execSync(''+ gcBuilder +' users skills delete '+ user.id +' '+ skillArr.id +'');
-            console.log('EXECUTING SCRIPT: '+ gcBuilder +' users skills delete '+ user.id +' '+ skillArr.id +'');
+            execSync('gc users skills delete '+ user.id +' '+ skillArr.id +'');
+            console.log('EXECUTING SCRIPT: gc users skills delete '+ user.id +' '+ skillArr.id +'');
             console.log('Removed Skill ' + skill + ' for user ' + user.name+'');
 
             // Bulk add skills for each user
-            execSync(''+ gcBuilder +' users skills bulkremove '+ user.id +' -f ./bulk-add-skills.json');
-            console.log('EXECUTING SCRIPT: '+ gcBuilder +' users skills bulkremove '+ user.id +' -f ./bulk-add-skills.json');
+            execSync('gc users skills bulkremove '+ user.id +' -f ./bulk-add-skills.json');
+            console.log('EXECUTING SCRIPT: gc users skills bulkremove '+ user.id +' -f ./bulk-add-skills.json');
             console.log('Added Skills for user ' + user.name+'');
 
             // bulk-add-skills.json file contents
