7fe03e36d6a5c9855c762ab89c4d6797178b775b
john.carnell@genesys.com <john.carnell@genesys.com>
2021-10-28 15:12:18 -0400

    NO-JIRA: Adding some example transforms to the quick hits repository.

---
+++ users/transforms/convert-users-to-csv-with-transform.sh
@@ -0,0 +1,12 @@
+# >> START convert-users-to-csv-with-transform.csv Use the experimental transform feature in the CLI along with a Go template to create CSV file
+
+########
+#  This template file will be used to transform all users who have the title of Agent from a JSON to CSV format (Select fields)
+#######
+
+# Example transforms template.  Place this content into a file called users_to_csv.tmpl
+{{printf "id,email,title,division,acdAutoAnswer\n" }}
+{{- range . -}}{{if eq "Agent" .title}}{{printf "'%s','%s','%s','%s','%t'\n" .id .email .title .division.name .acdAutoAnswer}}{{end}}{{end}}
+
+# GC CLI command invoking the template 
+gc users list -a --transform code/users_to_csv.tmpl
