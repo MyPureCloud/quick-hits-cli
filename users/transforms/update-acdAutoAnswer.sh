47e25521f56f015a506a21ddcaedb206882c50fc
John Carnell <john_carnell@yahoo.com>
2021-10-29 09:53:29 -0400

    Update update-acdAutoAnswer.sh

--- users/transforms/update-acdAutoAnswer.sh
+++ users/transforms/update-acdAutoAnswer.sh
@@ -1,4 +1,4 @@
-# >> START update-acdAutoAnswer.sh Use the experimental transform feature in the CLI along with a Go template to filter and then do a bulk update of the autoAcdAnswer 
+# >> START update-acdAutoAnswer Use the experimental transform feature in the CLI along with a Go template to filter and then do a bulk update of the autoAcdAnswer 
 
 ########
 #  This template file will be used to identify all users who have "Agent" in their title and whose acdAutoAnswer function is set to false.  It will then build a JSON body that
@@ -20,3 +20,5 @@ gc users list -a --transform acdautoansweron_bulk.tmpl
 
 # To transform the output and then redirect it to the bulk update.  Remember the gc users bulk update API can only update a total of 50 records.
 gc users list -a --transform acdautoansweron_bulk.tmpl | gc users bulk update
+
+# >> END convert-users-to-csv-with-transform
